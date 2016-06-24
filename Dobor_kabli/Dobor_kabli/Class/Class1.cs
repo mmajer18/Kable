d07dd0146ccc9c2b81ce9a5cf26c6e791648a066
mmajer18 <marek.majer@e-bpm.pl>
2016-06-24 18:28:00 +0200

    Dodano klasę obwód

---
+++ Dobor_kabli/Dobor_kabli/Class/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dobor_kabli.Class
+{
+    class obwod
+    {
+        int cableLength; //in meters
+        string isolation; //PVC or XLPE
+        double resistivity; //resistivity ohm/km
+        double resistance; //ohm
+        double reactance;
+        double reaktanceDivKm;
+        double maxTemp;
+        double maxFaultTemp;
+        double metodOfInstallation;
+        double numberOfCables;
+        double reduceingFactor;
+        double load;
+        double current;
+    }
+}
